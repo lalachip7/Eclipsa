1551c8ef10fbf7a809b4280072394c71965d7b67
mariannajimenezv <127933982+mariannajimenezv@users.noreply.github.com>
2025-11-25 21:11:30 +0100

    MenuScene

--- Juego/src/scenes/MenuScene.js
+++ Juego/src/scenes/MenuScene.js
@@ -5,7 +5,15 @@ export class MenuScene extends Phaser.Scene {
         super('MenuScene');
     }
 
+    preload() {
+        // fondo del menú
+        this.load.image('Pantalla_de_inicio', '../assets/Pantalla_de_inicio.PNG');
+    }
     create() {
+        const bg = this.add.image(0, 0, 'Pantalla_de_inicio').setOrigin(0);
+        bg.setDisplaySize(this.scale.width, this.scale.height);
+        bg.setDepth(-1);
+
         this.add.text(400, 100, 'PONG GAME', 
             { fontSize: '64px', fill: '#b9f5daff' }).setOrigin(0.5);
 

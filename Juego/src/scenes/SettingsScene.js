dc5b8ecd9da7067ddb2241608749a45aef3da0c8
Samuelovich <samuelretamero@outlook.com>
2025-12-01 18:38:42 +0100

    UI de Volumen
    me daba error al crear funciones, funciona, pero he dejado el codigo feo y poco legible

--- Juego/src/scenes/SettingsScene.js
+++ Juego/src/scenes/SettingsScene.js
@@ -10,10 +10,15 @@ export class SettingsScene extends Phaser.Scene {
         this.load.image('settingsBox', 'assets/caja.png');
 
         // Boton Volumen (+/-)
-        this.load.image('SettingsButton', 'assets/ajustes.png');
-        this.load.image('SettingsButtonHover', 'assets/ajustesHover.png');
+        this.load.image('PlusVolumeButton', 'assets/ajustes.png');
+        this.load.image('PlusVolumeHover', 'assets/ajustesHover.png');
+        this.load.image('MinusVolumeButton', 'assets/ajustes.png');
+        this.load.image('MinusVolumeHover', 'assets/ajustesHover.png');
 
         // Barras Volumen (rectangulos uno oscuro/apagados y otro claro/encendidos)
+        this.load.image('VolumeBarOff', 'assets/ajustes.png');
+        this.load.image('VolumeBarOn', 'assets/ajustesHover.png');
+        
 
         // Botón de salir (x)
         this.load.image('ExitMinButton', 'assets/cerrar.png');
@@ -25,7 +30,15 @@ export class SettingsScene extends Phaser.Scene {
         const w = this.scale.width;
         const h = this.scale.height;
 
+        // Volumen
+        
+        //let volume = this.scene.sound.volume;
+        let volume = 60;
+        //if(volume > 100) volume = 100;
+		//else if(volume < 0) volume = 0;
+
         let hoverImg = null;    //refrencia para la imagen hover
+        let volumeBar1, volumeBar2, volumeBar3, volumeBar4, volumeBar5;
 
         // Rectángulo que cubre toda la pantalla
         this.background = this.add.rectangle(0, 0, w, h, 0x070722, 0.9).setOrigin(0);
@@ -35,25 +48,259 @@ export class SettingsScene extends Phaser.Scene {
             .setOrigin(0.5)
             .setScale(1);
 
-        // Volumen aumentado
-        const plusVolumeBtn = this.add.image(400, 500, 'SettingsButton')
+        // Texto Volumen
+        this.volumeText = this.add.text(700, 300, volume,{ 
+            fontSize: '48px', fill: '#d4eaf1ff'}).setOrigin(0.5);
+        // Barras de volumen fondo
+        this.add.image(433, 500, 'VolumeBarOff')
+            .setOrigin(0.5)
+            .setScale(1);
+        this.add.image(566, 500, 'VolumeBarOff')
+            .setOrigin(0.5)
+            .setScale(1);
+        this.add.image(700, 500, 'VolumeBarOff')
+            .setOrigin(0.5)
+            .setScale(1);
+        this.add.image(833, 500, 'VolumeBarOff')
+            .setOrigin(0.5)
+            .setScale(1);
+        this.add.image(966, 500, 'VolumeBarOff')
+            .setOrigin(0.5)
+            .setScale(1);
+
+        // Barras de volumen activas 
+        volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        volumeBar5 = this.add.image(966, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        
+
+        // Actualizar barras de volumen 
+        volumeBar1.destroy();
+        volumeBar2.destroy();
+        volumeBar3.destroy();
+        volumeBar4.destroy();
+        volumeBar5.destroy();
+        if(volume == 0 && volumeBar1 != null){
+            volumeBar1.destroy();
+        }else if(volume <= 20){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume <= 40){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume <= 60){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume <= 80){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume == 100){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar5 = this.add.image(966, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }
+        
+        // Boton volumen aumentado
+        const plusVolumeBtn = this.add.image(1100, 500, 'PlusVolumeButton')
             .setOrigin(0.5)
             .setScale(0.7)
             .setInteractive({ useHandCursor: true });
         
         plusVolumeBtn.on('pointerdown', () => {
-            //if volume < 1, volume +=0.2
+            volume += 20;
+            if(volume > 100) volume = 100;
+		    else if(volume < 0) volume = 0;
+
+            //this.scene.sound.setVolume(volume);
+		    this.volumeText.destroy();
+            this.volumeText = this.add.text(700, 300, volume,{ 
+            fontSize: '48px', fill: '#d4eaf1ff'}).setOrigin(0.5);
+
+            //updateVolumeBars(volume);
+            volumeBar1.destroy();
+            volumeBar2.destroy();
+            volumeBar3.destroy();
+            volumeBar4.destroy();
+            volumeBar5.destroy();
+            if(volume == 0 && volumeBar1 != null){
+                volumeBar1.destroy();
+            }else if(volume <= 20){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume <= 40){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume <= 60){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume <= 80){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume == 100){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar5 = this.add.image(966, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }
         });
 
-        // Volumen disminuido
-        const minusVolumeBtn = this.add.image(1000, 500, 'SettingsButton')
+        // Boton volumen disminuido
+        const minusVolumeBtn = this.add.image(300, 500, 'MinusVolumeButton')
             .setOrigin(0.5)
             .setScale(0.7)
             .setInteractive({ useHandCursor: true });
 
         minusVolumeBtn.on('pointerdown', () => {
-            //if volume > 0, volume -=0.2
+		    volume -= 20;
+            if(volume > 100) volume = 100;
+		    else if(volume < 0) volume = 0;
+
+            //this.scene.sound.setVolume(volume);
+            this.volumeText.destroy();
+		    this.volumeText = this.add.text(700, 300, volume,{ 
+            fontSize: '48px', fill: '#d4eaf1ff'}).setOrigin(0.5);
+
+            //updateVolumeBars(volume);
+            volumeBar1.destroy();
+            volumeBar2.destroy();
+            volumeBar3.destroy();
+            volumeBar4.destroy();
+            volumeBar5.destroy();
+            if(volume == 0 && volumeBar1 != null){
+                volumeBar1.destroy();
+            }else if(volume <= 20){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume <= 40){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume <= 60){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume <= 80){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }else if(volume == 100){
+                volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+                volumeBar5 = this.add.image(966, 500, 'VolumeBarOn')
+                .setOrigin(0.5)
+                .setScale(1);
+            }
         });
+
+
 
         // Botón de salir (x)
         const ExitBtn = this.add.image(1069, 170, 'ExitMinButton')
@@ -91,4 +338,64 @@ export class SettingsScene extends Phaser.Scene {
 
         
     }
+    // Actualizar los sprites de volumen
+    /*
+    updateVolumeBars(volume){
+        volumeBar1.destroy();
+        volumeBar2.destroy();
+        volumeBar3.destroy();
+        volumeBar4.destroy();
+        volumeBar5.destroy();
+        if(volume <= 0.2){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume <= 0.4){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume <= 0.6){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume <= 0.8){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }else if(volume == 1){
+            volumeBar1 = this.add.image(433, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar2 = this.add.image(566, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar3 = this.add.image(700, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar4 = this.add.image(833, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+            volumeBar5 = this.add.image(966, 500, 'VolumeBarOn')
+            .setOrigin(0.5)
+            .setScale(1);
+        }
+    }*/
 }
